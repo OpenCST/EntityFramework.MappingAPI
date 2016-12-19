b20942ac9a6c5566a78d193d58858cf39c6fadd8
Antonio Anzivino <antonio.anzivino@csttech.it>
2016-12-19 11:43:37 +0100

    Porting of source code https://efmappingapi.svn.codeplex.com/svn 2016-12-19

---
+++ EntityFramework.MappingAPI/Exceptions/ParentNotMappedYetException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityFramework.MappingAPI.Exceptions
+{
+    public class ParentNotMappedYetException : Exception
+    {
+    }
+}
